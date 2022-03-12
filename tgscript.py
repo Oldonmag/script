801589927c53b675d2191e4ec66f568eda888c98
Pr0n1xGH <101285149+Pr0n1xGH@users.noreply.github.com>
2022-03-13 01:26:17 +0500

    Add files via upload
    v0.8.1 | added several commands.

--- tgscript.py
+++ tgscript.py
@@ -16,13 +16,17 @@ if os.sys.platform == "win32":
     os.system("cls")
 else:
     os.system("clear")
-print('''   v0.8
-      ┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓
-      ┃-------Made by PrOn1x--------┃
-      ┃------Telegram: @tgscriptss--┃
-      ┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛
+print('''
 
+██╗░░████████╗░██████╗░░██████╗░█████╗░██████╗░██╗██████╗░████████╗░░░██████╗░██╗░░░██╗
+╚██╗░╚══██╔══╝██╔════╝░██╔════╝██╔══██╗██╔══██╗██║██╔══██╗╚══██╔══╝░░░██╔══██╗╚██╗░██╔╝
+░╚██╗░░░██║░░░██║░░██╗░╚█████╗░██║░░╚═╝██████╔╝██║██████╔╝░░░██║░░░░░░██████╔╝░╚████╔╝░
+░██╔╝░░░██║░░░██║░░╚██╗░╚═══██╗██║░░██╗██╔══██╗██║██╔═══╝░░░░██║░░░░░░██╔═══╝░░░╚██╔╝░░
+██╔╝░░░░██║░░░╚██████╔╝██████╔╝╚█████╔╝██║░░██║██║██║░░░░░░░░██║░░░██╗██║░░░░░░░░██║░░░
+╚═╝░░░░░╚═╝░░░░╚═════╝░╚═════╝░░╚════╝░╚═╝░░╚═╝╚═╝╚═╝░░░░░░░░╚═╝░░░╚═╝╚═╝░░░░░░░░╚═╝░░░
+																				  v.0.8.1
 ''')
+
 print("После ввода задержки напишите в любой телеграм чат команду -help для просмотра команд!")
 print("\n МЫ НЕ НЕСЕМ ОТВЕТСВЕННОСТИ ЗА ВАШИ ДЕЙСТВИЯ!")
 print()
@@ -1761,6 +1765,189 @@ def betalove(_, msg):
         number = number + 1
         msg.edit(f'<b> </b>')
         msg.edit(f'<b> </b>')
+
+@app.on_message(filters.command("showdown", prefixes=".") & filters.me)
+def valentine(app, msg):
+    msg.edit(f"<b>Начало через: 13s</b>")  # orange
+    sleep(0.6)
+    msg.edit(f"<b>Начало через: 12s</b>")  # red
+    sleep(0.6)
+    msg.edit(f"<b>Начало через: 11s</b>")  # orange
+    sleep(0.6)
+    msg.edit(f"<b>Начало через: 10s</b>")  # red
+    sleep(0.6)
+    msg.edit(f"<b>Начало через: 9s</b>")  # orange
+    sleep(0.6)
+    msg.edit(f"<b>Начало через: 8s</b>")  # red
+    sleep(0.6)
+    msg.edit(f"<b>Начало через: 7s</b>")  # orange
+    sleep(0.6)
+    msg.edit(f"<b>Начало через: 6s</b>")  # red
+    sleep(0.6)
+    msg.edit(f"<b>Начало через: 5s</b>")  # orange
+    sleep(0.6)
+    msg.edit(f"<b>Начало через: 4s</b>")  # red
+    sleep(0.6)
+    msg.edit(f"<b>Начало через: 3s</b>")  # orange
+    sleep(0.6)
+    msg.edit(f"<b>Начало через: 2s</b>")  # red
+    sleep(0.6)
+    msg.edit(f"<b>Начало через: 1s</b>")  # orange
+    sleep(0.2)
+    msg.edit(f"<b>Бу, блять! Ха-ха</b>") 
+    sleep(1.2)
+    msg.edit(f"<b>Просыпайтесь нахуй (Let's go!)</b>")  # orange
+    sleep(1.3)
+    app.send_message(msg.chat.id, f'''
+	<b>Головы сияют на моей едкой катане</b>
+	''')
+    sleep(1.3)
+    app.send_message(msg.chat.id, f'''
+    <b>Голоса этих ублюдков по пятам бегут за нами</b>
+    ''')
+    sleep(1.3)
+    app.send_message(msg.chat.id, f'''
+    <b>Погружённый в Изанами, все колёса под глазами</b>
+    ''')
+    sleep(1.3)
+    app.send_message(msg.chat.id, f'''
+    <b>Её взгляд убьёт любого, её взгляд убьёт цунами</b>
+    ''')
+    sleep(1.3)
+    app.send_message(msg.chat.id, f'''
+    <b>Похоронный марш гулей, на часах последний тик</b>
+    ''')
+    sleep(1.3)
+    app.send_message(msg.chat.id, f'''
+    <b>Моя тати — Бравл Шелли, я несу ей дробовик</b>
+    ''')
+    sleep(1.3)
+    app.send_message(msg.chat.id, f'''
+    <b>Ваши головы — мишени, я снесу их в один миг</b>
+    ''')
+    sleep(1.3)
+    app.send_message(msg.chat.id, f'''
+    <b>Никаких резких движений — ваш хилбар на один хит</b>
+    ''')
+    sleep(1.3)
+    app.send_message(msg.chat.id, f'''
+    <b>Динамайк трипл килл, ха, нервы на пределе</b>
+    ''')
+    sleep(1.3)
+    app.send_message(msg.chat.id, f'''
+    <b>Voice в моих ушах — я позабыл все дни недели</b>
+    ''')
+    sleep(1.3)
+    app.send_message(msg.chat.id, f'''
+    <b>Как на лезвии ножа и шквал патрон, летят шрапнели</b>
+    ''')
+    sleep(1.3)
+    app.send_message(msg.chat.id, f'''
+    <b>Psychokilla — весь мой шарм, вся эта мапа поредели</b>
+    ''')
+    sleep(1.5)
+    app.send_message(msg.chat.id, f'''
+    <b>Эй, погоди, мои парни на Стокгольме</b>
+    ''')
+    sleep(1.3)
+    app.send_message(msg.chat.id, f'''
+    <b>Мой showdown 1x1, и мои демоны все в форме</b>
+    ''')
+    sleep(1.3)
+    app.send_message(msg.chat.id, f'''
+    <b>Если я зайду к вам в лобби — оно станет вам могилой</b>
+    ''')
+    sleep(1.3)
+    app.send_message(msg.chat.id, f'''
+    <b>Если ты зайдешь — мне похуй, я не стартану и выйду, а-ха</b>
+    ''')
+    sleep(1.3)
+    app.send_message(msg.chat.id, f'''
+    <b>По приказу Генерала Гавса!</b>
+    ''')
+    sleep(1.4)
+    app.send_message(msg.chat.id, f'''
+    <b>— Бро, тут вообще сложная ситуация, все границы позакрывали нахуй. Ваще пиздец полный. Ща просто едем ближе ко Львову, но во Львове тоже пиздец начался, поэтому хуй знает</b>
+    ''')
+    sleep(1.9)
+    app.send_message(msg.chat.id, f'''
+    <b>— Бля, чуваки, шутки шутками, но не занимайтесь хуйнёй, я вас умоляю. А-а-а!</b>
+    ''')
+    sleep(1.3)
+    app.send_message(msg.chat.id, f'''
+    <b>Эй, я как Вольт — называй неуловимый</b>
+    ''')
+    sleep(1.3)
+    app.send_message(msg.chat.id, f'''
+    <b>Я в showdown'е, как Кольт — твои патроны летят мимо</b>
+    ''')
+    sleep(1.3)
+    app.send_message(msg.chat.id, f'''
+    <b>Ты на этой мапе — ноль, ты не скрывайся — тебя видно</b>
+    ''')
+    sleep(1.3)
+    app.send_message(msg.chat.id, f'''
+    <b>Я как Рико, дал обойму, мой лайфстайл — psychokilla</b>
+    ''')
+    sleep(1.3)
+    app.send_message(msg.chat.id, f'''
+    <b>De-Dead inside mode, я бегу по головам</b>
+    ''')
+    sleep(1.3)
+    app.send_message(msg.chat.id, f'''
+    <b>Оверсайз весь шмот, я на трапе тут и там</b>
+    ''')
+    sleep(1.3)
+    app.send_message(msg.chat.id, f'''
+    <b>Весь твой скилл — шаблон, я по рофлу на битах</b>
+    ''')
+    sleep(1.3)
+    app.send_message(msg.chat.id, f'''
+    <b>Зачем мне октагон? Могу выйти на финдах, ха</b>
+    ''')
+    sleep(1.3)
+    app.send_message(msg.chat.id, f'''
+	<b>Головы сияют на моей едкой катане</b>
+	''')
+    sleep(1.3)
+    app.send_message(msg.chat.id, f'''
+    <b>Голоса этих ублюдков по пятам бегут за нами</b>
+    ''')
+    sleep(1.3)
+    app.send_message(msg.chat.id, f'''
+    <b>Погружённый в Изанами, все колёса под глазами</b>
+    ''')
+    sleep(1.3)
+    app.send_message(msg.chat.id, f'''
+    <b>Её взгляд убьёт любого, её взгляд убьёт цунами</b>
+    ''')
+    sleep(1.3)
+    app.send_message(msg.chat.id, f'''
+    <b>Генерал Гавс, ха, вижу вас без гема</b>
+    ''')
+    sleep(1.3)
+    app.send_message(msg.chat.id, f'''
+    <b>Я отдал приказ, и все умрут от реквиема</b>
+    ''')
+    sleep(1.3)
+    app.send_message(msg.chat.id, f'''
+    <b>Дота-рэп — топ чарт, ха, наебал систему</b>
+    ''')
+    sleep(1.3)
+    app.send_message(msg.chat.id, f'''
+    <b>Mute all chat, я на лям скупил все гемы, ха-ха</b>
+    ''')
+    sleep(1.4)
+    app.send_message(msg.chat.id, f'''
+    <b>Ха-а, бля</b>
+    ''')
+
+    sleep(0.5)
+    global number
+    number = number + 1
+    app.send_message(message.chat.id, f'''
+     <b> </b>
+     ''')
 
 
 @app.on_message(filters.command("toxic", prefixes=".") & filters.me)
